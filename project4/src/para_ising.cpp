49cf7934ae0878758f545c00e1a71095ad26feac
Audun <auduntre@student.matnat.uio.no>
2018-11-18 22:47:41 +0100

    parallelization on temperature instead of mcc

--- project4/src/para_ising.cpp
+++ project4/src/para_ising.cpp
@@ -82,18 +82,12 @@ int main (int argc, char* argv[])
     MPI_Bcast (&TempStep, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     MPI_Bcast (&NTempSteps, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-    vec Temps = linspace<vec>(InitialTemp, FinalTemp, NTempSteps);
-
-    // Informing if temperature step is changed
-    if (RankProcess == 0 && (Temps[1] - Temps[0] != TempStep)) {
-        cout << "New temperature step: " << Temps[1] - Temps[0] << endl;
-    }
-
+    // Step dividers
     int rankSteps = NTempSteps / NProcesses;
     int reminderSteps = NTempSteps % NProcesses;
 
-    int startStep;
-    int endStep;
+    int startStep = 0;
+    int endStep = 0;
     int commStep;
 
     // Divinding Temps between processes
@@ -101,50 +95,62 @@ int main (int argc, char* argv[])
 
     if (RankProcess <= reminderSteps) {
         startStep = (rankSteps + 1) * RankProcess;
-    }
-    else {
+    } else {
         startStep = (rankSteps + 1) * reminderSteps
                   + rankSteps * (RankProcess - reminderSteps); 
     }
 
+    // Sending start step to process rank - 1  
     if (RankProcess > 0) {
         commStep = startStep;
-        MPI_Send (&commStep, 1, MPI_INT, RankProcess - 1, 0,
-                  MPI_COMM_WORLD);
+        MPI_Send(&commStep, 1, MPI_INT, RankProcess - 1, 0,
+                 MPI_COMM_WORLD);
     }
 
+    // Recieve proces (rank + 1)'s start step to set own end step 
     if (RankProcess < NProcesses - 1 && NProcesses > 1) {
-        MPI_Recv (&commStep, 1, MPI_INT, RankProcess + 1, 0, 
-                  MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(&commStep, 1, MPI_INT, RankProcess + 1, 0, 
+                 MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         endStep = commStep - 1;
     }
 
-    cout << "Rank: " << RankProcess << ", Start Step: " << startStep << ", End Step: " << endStep << endl;
+    vec temps = linspace<vec>(InitialTemp, FinalTemp, NTempSteps);
+    mat totalExpectationValues(5, NTempSteps, fill::zeros);
+    mat localExpectationValues(5, NTempSteps, fill::zeros);
+    
+    // Informing if temperature step is changed
+    if (RankProcess == 0 && (temps[1] - temps[0] != TempStep)) {
+        cout << "New temperature step: " << temps[1] - temps[0] << endl;
+    }
 
     // Start Monte Carlo sampling by looping over the selected Temperatures
     double  TimeStart, TimeEnd, TotalTime;
     TimeStart = MPI_Wtime();
 
+    for (int step = startStep; step <= endStep; step++) {
+        vec tmpEV = zeros<vec>(5);
 
-    for (double Temperature = InitialTemp; Temperature <= FinalTemp; Temperature+=TempStep) {
-        vec LocalExpectationValues = zeros<mat>(5);
-        
         // Start Monte Carlo computation and get local expectation values
-        MetropolisSampling(NSpins, MonteCarloCycles, Temperature, LocalExpectationValues);
-        
-        // Find total average
-        vec TotalExpectationValues = zeros<mat>(5);
-        
-        for (int i = 0; i < 5; i++){
-            MPI_Reduce(&LocalExpectationValues[i], &TotalExpectationValues[i], 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-        }
-        
-        if (RankProcess == 0) {
-            WriteResultstoFile(ofile, NSpins, MonteCarloCycles * NProcesses, Temperature, TotalExpectationValues);
-        }
+        MetropolisSampling(NSpins, MonteCarloCycles, temps[step], 
+                           tmpEV);
+
+        localExpectationValues.col(step) = tmpEV;
     }
-    
-    if(RankProcess == 0)  ofile.close();  // close output file
+
+    // Send all values to process 0
+    for (int ij = 0; ij < 5 * NTempSteps; ij++) {
+        MPI_Reduce(&localExpectationValues(ij), &totalExpectationValues(ij),
+                   1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    }
+
+    // Print to file
+    if (RankProcess == 0) {
+        for (int step = 0; step < NTempSteps; step++) {
+            WriteResultstoFile(ofile, NSpins, MonteCarloCycles, temps[step], 
+                               totalExpectationValues.col(step));
+        }
+        ofile.close();  // close output file
+    }
     
     TimeEnd = MPI_Wtime();
     TotalTime = TimeEnd - TimeStart;
